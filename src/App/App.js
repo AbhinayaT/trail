acfdeb822950a3b0843fa31ad3923c3ce6fbd9c9
AbhinayaT <thirupathiabhinaya@gmail.com>
2021-04-10 03:28:54 +0530

    commit

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+
+import SideMenu from '../components/SideMenu';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <div>here we go</div>
+    <SideMenu/>
+    </div>
+    
+  );
+}
+
+export default App;
